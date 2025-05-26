0465f551ee332139c0dfafc6a42cda6d0612fb91
sharmaaakash170 <sharmaaakash170@gmail.com>
2025-05-27 01:42:56 +0530

    testing

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -5,7 +5,3 @@ output "eks_cluster_role_arn" {
 output "eks_node_group_role_arn" {
   value = aws_iam_role.eks_node_group_role.arn
 }
-
-output "service_account_role_arn" {
-  value = aws_iam_role.ebs_csi_driver_irsa_role.arn
-}
