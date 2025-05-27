b894c4f1c11d47d2c6beba968ff3656f86607ed9
sharmaaakash170 <sharmaaakash170@gmail.com>
2025-05-27 12:37:50 +0530

    Adding

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -14,6 +14,10 @@ output "cluster_sg_group_id" {
   value = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
 }
 
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.this.name
+}
+
 output "eks_cluster_token" {
-  value = aws_eks_cluster.this.token
+  value = data.aws_eks_cluster_auth.cluster.token
 }
