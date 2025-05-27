4a00edaf112ad5a5f97d1f27734c753b8437cea2
sharmaaakash170 <sharmaaakash170@gmail.com>
2025-05-27 12:30:56 +0530

    Adding

---
+++ terraform/environments/dev/ingress.tf
@@ -0,0 +1,33 @@
+resource "kubernetes_ingress_v1" "frontend" {
+  metadata {
+    name      = "frontend-ingress"
+    namespace = "default"
+
+    annotations = {
+      "kubernetes.io/ingress.class"                  = "alb"
+      "alb.ingress.kubernetes.io/scheme"             = "internet-facing"
+      "alb.ingress.kubernetes.io/target-type"        = "ip"
+      "alb.ingress.kubernetes.io/backend-protocol"   = "HTTP"
+    }
+  }
+
+  spec {
+    rule {
+      http {
+        path {
+          path      = "/*"
+          path_type = "ImplementationSpecific"
+
+          backend {
+            service {
+              name = "frontend"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ terraform/environments/dev/service.tf
@@ -0,0 +1,18 @@
+resource "kubernetes_service" "frontend" {
+  metadata {
+    name = "frontend"
+  }
+
+  spec {
+    selector = {
+      app = "frontend"
+    }
+
+    port {
+      port        = 80
+      target_port = 8081
+    }
+
+    type = "ClusterIP"
+  }
+}
