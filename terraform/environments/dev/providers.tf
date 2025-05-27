154b0ec462c43f10e230a56caf8f76697c41b823
sharmaaakash170 <sharmaaakash170@gmail.com>
2025-05-27 12:32:03 +0530

    Adding

--- terraform/environments/dev/providers.tf
+++ terraform/environments/dev/providers.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = var.aws_region
 }
 
-provider "kubernetes" {
-  host = module.eks.eks_cluster_name.endpoint
-  cluster_ca_certificate = base64encode(module.eks.cluster_ca_certificate[0].data)
-  token = module.eks.eks_cluster_name.token
-}
+# provider "kubernetes" {
+#   host = module.eks.eks_cluster_name.endpoint
+#   cluster_ca_certificate = base64encode(module.eks.cluster_ca_certificate[0].data)
+#   token = module.eks.eks_cluster_name.token
+# }
