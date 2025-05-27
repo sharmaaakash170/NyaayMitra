4c0e511b50ba450b989322dfc05665a210d18e0a
sharmaaakash170 <sharmaaakash170@gmail.com>
2025-05-27 23:32:18 +0530

    Updating token

--- terraform/environments/dev/providers.tf
+++ terraform/environments/dev/providers.tf
@@ -2,8 +2,14 @@ provider "aws" {
   region = var.aws_region
 }
 
+data "aws_eks_cluster_auth" "this" {
+  name = aws_eks_cluster.this.name
+}
+
 provider "kubernetes" {
   host = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_ca_certificate)
-  token = module.eks.eks_cluster_token
+
+  token = data.aws_eks_cluster_auth.this.token
+
 }
